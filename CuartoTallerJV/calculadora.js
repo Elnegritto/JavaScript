28a00937f89f1019002cb839f53886560e1bbe18
Elnegritto <kevinhp2006@gmail.com>
2023-11-21 11:25:48 -0500

    E

--- CuartoTallerJV/calculadora.js
+++ CuartoTallerJV/calculadora.js
@@ -1,77 +1,110 @@
-function sumar (n1, n2){
+// Función para sumar dos números y mostrar el resultado en un elemento HTML
+function sumar(n1, n2) {
+    // Calcular la suma de los dos números
     var resultado = n1 + n2;
-    var res = document.getElementById("resultado")
-    if (n1 != 0 && n2 != 0){
-        respuesta.style.display = "block"
+    // Obtener el elemento HTML donde se mostrará el resultado
+    var res = document.getElementById("resultado");
+    
+    // Verificar si ambos números son distintos de cero
+    if (n1 != 0 && n2 != 0) {
+        // Mostrar el resultado con formato y cambiar el color del texto a verde
+        respuesta.style.display = "block";
         res.innerHTML = `Total: ${resultado}`;
         res.style.color = "green";
-    }else{
+    } else {
+        // Mostrar mensaje de verificación de datos y cambiar el color del texto a rojo
         respuesta.style.display = "block";
         res.innerHTML = `Verifique sus datos`;
         res.style.color = "red";
     }
 }
 
-function restar (n1, n2){
+// Función para restar dos números y mostrar el resultado en un elemento HTML
+function restar(n1, n2) {
+    // Calcular la resta de los dos números
     var resultado = n1 - n2;
-    var res = document.getElementById("resultado")
-    if (n1 != 0 && n2 != 0){
-        respuesta.style.display = "block"
+    // Obtener el elemento HTML donde se mostrará el resultado
+    var res = document.getElementById("resultado");
+    
+    // Verificar si ambos números son distintos de cero
+    if (n1 != 0 && n2 != 0) {
+        // Mostrar el resultado con formato y cambiar el color del texto a verde
+        respuesta.style.display = "block";
         res.innerHTML = `Total: ${resultado}`;
         res.style.color = "green";
-    }else{
+    } else {
+        // Mostrar mensaje de verificación de datos y cambiar el color del texto a rojo
         respuesta.style.display = "block";
         res.innerHTML = `Verifique sus datos`;
         res.style.color = "red";
     }
 }
 
-function multiplicar (n1, n2){
+// Función para multiplicar dos números y mostrar el resultado en un elemento HTML
+function multiplicar(n1, n2) {
+    // Calcular el producto de los dos números
     var resultado = n1 * n2;
-    var res = document.getElementById("resultado")
-    if (n1 != 0 && n2 != 0){
-        respuesta.style.display = "block"
+    // Obtener el elemento HTML donde se mostrará el resultado
+    var res = document.getElementById("resultado");
+    
+    // Verificar si ambos números son distintos de cero
+    if (n1 != 0 && n2 != 0) {
+        // Mostrar el resultado con formato y cambiar el color del texto a verde
+        respuesta.style.display = "block";
         res.innerHTML = `Total: ${resultado}`;
         res.style.color = "green";
-    }else{
+    } else {
+        // Mostrar mensaje de verificación de datos y cambiar el color del texto a rojo
         respuesta.style.display = "block";
         res.innerHTML = `Verifique sus datos`;
         res.style.color = "red";
     }
 }
 
-function dividir (n1, n2){
+// Función para dividir dos números y mostrar el resultado en un elemento HTML
+function dividir(n1, n2) {
+    // Calcular la división de los dos números
     var resultado = n1 / n2;
-    var res = document.getElementById("resultado")
-    if (n1 != 0 && n2 != 0){
-        respuesta.style.display = "block"
+    // Obtener el elemento HTML donde se mostrará el resultado
+    var res = document.getElementById("resultado");
+    
+    // Verificar si ambos números son distintos de cero
+    if (n1 != 0 && n2 != 0) {
+        // Mostrar el resultado con formato y cambiar el color del texto a verde
+        respuesta.style.display = "block";
         res.innerHTML = `Total: ${resultado}`;
         res.style.color = "green";
-    }else{
+    } else {
+        // Mostrar mensaje de verificación de datos y cambiar el color del texto a rojo
         respuesta.style.display = "block";
         res.innerHTML = `Verifique sus datos`;
         res.style.color = "red";
     }
 }
 
-function mostrar(){
-    let n1 = parseInt(document.getElementById("n1").value)
-    let n2 = parseInt(document.getElementById("n2").value)
-    let opc = parseInt(document.getElementById("opc").value)
-    switch(opc){
+// Función para obtener los valores de entrada y realizar la operación seleccionada
+function mostrar() {
+    // Obtener los valores de los elementos de entrada
+    let n1 = parseInt(document.getElementById("n1").value);
+    let n2 = parseInt(document.getElementById("n2").value);
+    let opc = parseInt(document.getElementById("opc").value);
+
+    // Realizar la operación correspondiente según la opción seleccionada
+    switch (opc) {
         case 1:
-            sumar(n1, n2)
+            sumar(n1, n2);
             break;
         case 2:
-            restar(n1, n2)
+            restar(n1, n2);
             break;
         case 3:
-            multiplicar(n1, n2)
+            multiplicar(n1, n2);
             break;
         case 4:
-            dividir(n1, n2)
+            dividir(n1, n2);
             break;
         default:
-            alert("Opcion Inválida")
+            // Mostrar alerta en caso de opción inválida
+            alert("Opcion Inválida");
     }
 }
