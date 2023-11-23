184cea9f676adeef198eddaacfdcff8cb05ef767
Elnegritto <kevinhp2006@gmail.com>
2023-11-23 13:34:31 -0500

    Código completamente comentado

--- EjercicioEvaluativoJV/js/Carrito.js
+++ EjercicioEvaluativoJV/js/Carrito.js
@@ -1,13 +1,16 @@
+// Función para mostrar las opciones de compañías y ocultar el menú inicial
 function mostrarOpciones() {
     document.getElementById("opcionesMenu").style.display = "block";
     document.getElementById("menu").style.display = "none";
     document.getElementById("contenido").style.display = "block";
 }
 
+// Función para calcular el total a pagar según la compañía y la cantidad de planes
 function calcularTotal() {
     var telefonia = document.getElementById("telefonia").value;
-    var cantidad = parseInt(prompt("Ingrese la cantidad de planes que desea alquirir el dia hoy,(SI QUIERE VER EL PRECIO UNITARIO DE DICHO PLAN DIGITE 0)"));
+    var cantidad = parseInt(prompt("Ingrese la cantidad de planes que desea alquilar hoy (si desea ver el precio unitario, digite 0)"));
 
+    // Definición de precios unitarios por compañía
     switch (telefonia) {
         case "Tigo":
             var precioUnitario = 36000; // Precio de un plan de Tigo
@@ -19,7 +22,7 @@ function calcularTotal() {
             var precioUnitario = 30000; // Precio de un plan de Wow
             break;
         case "Virgin Mobile":
-            var precioUnitario = 28500; // Precio de un plan de Virgilio Móvil
+            var precioUnitario = 28500; // Precio de un plan de Virgin Mobile
             break;
         case "Movistar":
             var precioUnitario = 35400; // Precio de un plan de Movistar
@@ -29,11 +32,13 @@ function calcularTotal() {
             return;
     }
 
+    // Cálculo del total a pagar
     var totalPagar = cantidad * precioUnitario;
 
     // Mostrar los datos del pedido y el total a pagar
-    var datosPedido = `Compañía: ${telefonia}\nLa Cantidad De Planes Adquiridos Por Esta Compañía Es De: ${cantidad}\nPrecio Del Plan Unitario(Mes): ${precioUnitario}\nTotal a Pagar: ${totalPagar}`;
+    var datosPedido = `Compañía: ${telefonia}\nCantidad de planes: ${cantidad}\nPrecio Unitario (Mes): ${precioUnitario}\nTotal a Pagar: ${totalPagar}`;
 
     document.getElementById("pedido").innerText = datosPedido;
-    document.getElementById("total").innerText = `Total a Pagar Por su Plan De Telefonia Seria De: $${totalPagar}`;
+    document.getElementById("total").innerText = `Total a Pagar por tu Plan de Telefonía: $${totalPagar}`;
 }
+
